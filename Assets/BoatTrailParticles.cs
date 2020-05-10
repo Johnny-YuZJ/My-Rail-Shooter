5b73db474743551bb66d305f1da467f997ae1963
Johnny-Zhuojun-Yu <shanghaiboy1122@hotmail.com>
2020-05-09 22:50:53 -0400

    anything until particle effects

---
+++ Assets/BoatTrailParticles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class BoatTrailParticles : MonoBehaviour {
+
+    [SerializeField] float positionRollFactor = 0.4f;
+    [SerializeField] float controlRollFactor = 5;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ProcessRotation();
+    }
+
+    private void ProcessRotation() {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float rollDueToPosition = positionRollFactor * transform.localPosition.x;
+        float rollDueToControl = controlRollFactor * xThrow;
+        float roll = rollDueToPosition + rollDueToControl;
+        transform.localRotation = Quaternion.Euler(0, 90, roll);
+    }
+}
